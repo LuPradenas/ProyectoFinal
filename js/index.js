6cd35d906b4808f467c57aa573b3e10805ca300d
lupradenas <lucianapradenas3@gmail.com>
2023-09-13 20:12:26 -0300

    hice funcionar hasta calcular prestamo

--- js/index.js
+++ js/index.js
@@ -1,90 +1,46 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const seccionContainer = document.getElementById("section-container");
-  const btnSiguiente = document.getElementById("btn-next");
-  const btnPersona = document.getElementById("btn-persona");
-  const btnEmpresa = document.getElementById("btn-empresa");
+  let datos = {
+    nombreCompleto: undefined,
+    dni: undefined,
+    nombreEmpresa: undefined,
+    cuit: undefined,
+    tipoDeUsario: undefined,
+  };
 
-  let seccionActual = 1;
-  const maxSecciones = 1;
-  let tipoUsuario;
+  const contenedor = document.getElementById("section-container"); // referencia del contendor donde cargo las secciones que muestro al usuario
 
-  let nombreCompleto;
-  let dni;
-
-  btnPersona.addEventListener("click", () => {
-    tipoUsuario = "persona";
-    cargarSeccion();
-  });
-
-  btnEmpresa.addEventListener("click", () => {
-    tipoUsuario = "empresa";
-    cargarSeccion();
-  });
-
-  btnSiguiente.addEventListener("click", () => {
-    const datos = {
-      nombreCompleto: nombreCompleto.value,
-      dni: Number(dni.value),
+  const botonPersonal = document.getElementById("btn-persona");
+  botonPersonal.addEventListener("click", () => {
+    datos = {
+      ...datos,
+      tipoDeUsario: "persona",
     };
-    const usuarioEncontrado = usuarios.filter((user) => {
-      return (
-        user.nombreCompleto === datos.nombreCompleto && user.dni === datos.dni
-      );
-    });
-
-    if (usuarioEncontrado.length > 0) {
-      Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: "El nombre ingresado ya solicitó un préstamo. En caso de que no sea así, comuníquese al número 0800 555 333.",
-      });
-    } else {
-      cargarSiguienteSeccion();
-    }
+    cargarSeccion();
   });
 
-  function asignarEventListeners() {
-    nombreCompleto = document.getElementById("nombreCompleto");
-    dni = document.getElementById("dni");
-    nombreEmpresa = document.getElementById("nombreEmpresa");
-    cuit = document.getElementById("cuit");
-
-    nombreCompleto.addEventListener("input", habilitarButtonSiguiente);
-    dni.addEventListener("input", habilitarButtonSiguiente);
-    nombreEmpresa.addEventListener("input", habilitarButtonSiguiente);
-    cuit.addEventListener("input", habilitarButtonSiguiente);
-  }
-
-  function habilitarButtonSiguiente() {
-    if (
-      (nombreCompleto.value.trim() !== "" && dni.value.trim() !== "") ||
-      (nombreEmpresa.value.trim() !== "" && cuit.value.trim() !== "")
-    ) {
-      btnSiguiente.disabled = false;
-    } else {
-      btnSiguiente.disabled = true;
-    }
-  }
-
+  const botonEmpresa = document.getElementById("btn-empresa");
+  botonEmpresa.addEventListener("click", () => {
+    datos = {
+      ...datos,
+      tipoUsuario: "empresa",
+    };
+    cargarSeccion();
+  });
   function cargarSeccion() {
-    if (tipoUsuario === "persona") {
+    if (datos.tipoDeUsario === "persona") {
       fetch(`section/seccionPersona.html`)
         .then((response) => response.text())
         .then((data) => {
-          seccionContainer.innerHTML = data;
-          asignarEventListeners();
-          habilitarButtonSiguiente();
+          contenedor.innerHTML = data;
         })
         .catch((error) =>
           console.error("Error al cargar la sección persona:", error)
         );
-    } else if (tipoUsuario === "empresa") {
+    } else if (datos.tipoDeUsario === "empresa") {
       fetch(`section/seccionEmpresa.html`)
         .then((response) => response.text())
         .then((data) => {
-          seccionContainer.innerHTML = data;
-          asignarEventListeners();
-          habilitarButtonSiguiente();
+          contenedor.innerHTML = data;
         })
         .catch((error) =>
           console.error("Error al cargar la sección empresa:", error)
@@ -92,78 +48,132 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
+  const botonSiguiente = document.getElementById("btn-next"); // referencia del boton para navegar a proximas secciones desde que selecciono personal / empresa
+  botonSiguiente.addEventListener("click", cargarSiguienteSeccion);
   function cargarSiguienteSeccion() {
-    if (seccionActual <= maxSecciones) {
-      fetch(`section/section${seccionActual}.html`)
+    if (datos.tipoDeUsario === "persona") {
+      datos = {
+        ...datos,
+        nombreCompleto: document.getElementById("nombreCompleto").value,
+        dni: Number(document.getElementById("dni").value),
+      };
+    } else {
+      datos = {
+        ...datos,
+        nombreEmpresa: document.getElementById("nombreEmpresa").value,
+        cuit: Number(document.getElementById("cuit").value),
+      };
+    }
+
+    const usuarioLlenoLosInputs =
+      (datos.nombreCompleto?.trim() !== "" && datos.dni !== undefined) ||
+      (datos.nombreEmpresa?.trim() !== "" && datos.cuit !== undefined);
+
+    if (!usuarioLlenoLosInputs) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Necesitas completar los inputs",
+      });
+      return;
+    }
+
+    const usuarioEncontrado = usuarios.some((user) => {
+      return (
+        user.nombreCompleto === datos.nombreCompleto && user.dni === datos.dni
+      );
+    });
+
+    const empresaEncontrada = empresas.some((empresa) => {
+      return (
+        empresa.nombreEmpresa === datos.nombreEmpresa &&
+        empresa.cuit === datos.cuit
+      );
+    });
+
+    if (usuarioEncontrado || empresaEncontrada) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Ya solicitó un préstamo. En caso de que no sea así, comuníquese al número 0800 555 333. (profe no llame)",
+      });
+    } else {
+      let datosParaGuardar;
+      if (!usuarioEncontrado) {
+        datosParaGuardar = [...usuarios, datos];
+      } else if (!empresaEncontrada) {
+        datosParaGuardar = [...empresas, datos];
+      }
+      localStorage.setItem("usuarios", JSON.stringify(datosParaGuardar));
+
+      fetch(`section/section1.html`)
         .then((response) => response.text())
         .then((data) => {
-          seccionContainer.innerHTML = data;
-          asignarEventListeners();
-          habilitarButtonSiguiente();
+          contenedor.innerHTML = data;
         })
         .catch((error) => console.error("Error al cargar la sección:", error));
-      seccionActual++;
     }
   }
+
+  function simularPrestamo() {
+    const montoInput = document.getElementById("monto");
+    const interesInput = document.getElementById("interes");
+    const plazoInput = document.getElementById("plazo");
+
+    const monto = parseFloat(montoInput.value);
+    const interes = parseFloat(interesInput.value);
+    const plazo = parseInt(plazoInput.value);
+
+    console.log("monto:", monto);
+    console.log("plazo", plazo);
+    console.log("interes", interes);
+
+    if (!isNaN(monto) && !isNaN(interes) && !isNaN(plazo) && plazo > 0) {
+      Swal.fire({
+        icon: "warning",
+        title: "Oops...",
+        text: "Por favor ingrese valores validos.",
+      });
+    } else {
+      let cuota = calcularCuota(monto, interes, plazo).toFixed(2);
+      Swal.fire({
+        title: "Resultado de solicitud de préstamo",
+        html: `
+        <p>Monto ingresado: ${monto}</p>
+        <p>Tasa de interés: ${interes}</p>
+        <p>Cuotas: ${plazo} meses</p>
+        <p>La cuota de tu préstamo es:${cuota}pesos</p>
+      `,
+        showCancelButton: true,
+        confirmButtonText: "Aceptar",
+      }).then(async (result) => {
+        if (result.isConfirmed) {
+          const { value: email } = await Swal.fire({
+            title: "Ingresa tu email",
+            input: "email",
+            inputLabel: "Ingresa tu email",
+            inputPlaceholder: "Ingresa tu email",
+          });
+
+          if (email) {
+            Swal.fire("Solicitud enviada!", "", "success");
+          }
+        }
+      });
+      botonSiguiente.disabled = false;
+    }
+  }
+
+  function calcularCuota(monto, interes, plazo) {
+    // Calculo de tasa mensual
+    const tasaMensual = interes / 12;
+
+    // Calculo de cuota pura
+    const cuotaPura = monto / plazo;
+
+    // Calculo de cuota con interes mensual
+    const cuotaConInteres = cuotaPura + (cuotaPura * tasaMensual) / 100;
+
+    return cuotaConInteres;
+  }
 });
-
-// function simularPrestamo() {
-//   let repetir = true;
-//   while (repetir) {
-//     const deseaCalcular = prompt(
-//       "¿Deseas calcular la cuota de un préstamo? (si/no)"
-//     );
-
-//     if (deseaCalcular === "si") {
-//       // Obtener datos ingresados por el usuario
-//       let monto = parseInt(prompt("ingrese monto inicial del préstamo"));
-//       let interes = parseFloat(prompt("ingrese monto de interés anual (%)"));
-//       let plazo = parseInt(prompt("ingrese plazo de meses a financiar"));
-//       // Validar los datos ingresados por el usuario
-//       if (isNaN(monto) || isNaN(interes) || isNaN(plazo)) {
-//         alert("Por favor, ingrese valores validos.");
-//       } else {
-//         let cuota = calcularCuota(monto, interes, plazo).toFixed(2); // Redondear a dos decimales
-//         alert(`La cuota de tu préstamo es: $ ${cuota} pesos`);
-//         este alert de aca seria esta logica de codigo
-
-// Swal.fire({
-//   title: "Resultado de solicitud de préstamo",
-//   html: `
-//     <p>Monto ingresado: $monto_pesos</p>
-//     <p>Tasa de interés: $tasa_interes%</p>
-//     <p>Cuotas: $plazo meses</p>
-//     <p>La cuota de tu préstamo es: $numero_pesos pesos</p>
-//   `,
-//   showCancelButton: true,
-//   confirmButtonText: "Aceptar",
-// }).then(async (result) => {
-//   if (result.isConfirmed) {
-//     const { value: email } = await Swal.fire({
-//       title: "Ingresa tu email",
-//       input: "email",
-//       inputLabel: "Ingresa tu email",
-//       inputPlaceholder: "Ingresa tu email",
-//     });
-
-//     if (email) {
-//       Swal.fire("Solicitud enviada!", "", "success");
-//     }
-//   }
-// });
-
-// else{
-//       Swal.fire("Gracias por usar el simulador de préstamos.", "", "success");
-// }
-// function calcularCuota(monto, interes, plazo) {
-//   // Calculo de tasa mensual
-//   const tasaMensual = interes / 12;
-
-//   // Calculo de cuota pura
-//   const cuotaPura = monto / plazo;
-
-//   // Calculo de cuota con interes mensual
-//   const cuotaConInteres = cuotaPura + (cuotaPura * tasaMensual) / 100;
-
-//   return cuotaConInteres;
-// }
